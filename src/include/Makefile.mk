fa82eadbe8b1b546f73327e5efb7ae7eb80d49a7
Halim Amer <aamer@anl.gov>
2016-06-24 18:18:14 -0500

    added an embedded mode during configuration

--- src/include/Makefile.mk
+++ src/include/Makefile.mk
@@ -5,17 +5,23 @@
 
 prefixdir = ${prefix}
 
-nobase_prefix_HEADERS = \
-			include/common/zm_common.h \
-			include/mem/zm_hzdptr.h \
-			include/lock/zm_lock_types.h \
-			include/lock/zm_ticket.h \
-			include/lock/zm_mcs.h \
-			include/lock/zm_csvmcs.h \
-			include/list/zm_sdlist.h \
-			include/queue/zm_queue_types.h \
-			include/queue/zm_glqueue.h \
-			include/queue/zm_msqueue.h
+zm_headers = \
+	include/common/zm_common.h \
+	include/mem/zm_hzdptr.h \
+	include/lock/zm_lock_types.h \
+	include/lock/zm_ticket.h \
+	include/lock/zm_mcs.h \
+	include/lock/zm_csvmcs.h \
+	include/list/zm_sdlist.h \
+	include/queue/zm_queue_types.h \
+	include/queue/zm_glqueue.h \
+	include/queue/zm_msqueue.h
 
-noinst_HEADERS = \
+if ZM_EMBEDDED_MODE
+noinst_HEADERS = ${zm_headers}
+else
+noinst_HEADERS =
+nobase_prefix_HEADERS = ${zm_headers}
+endif
+noinst_HEADERS += \
 		include/zm_config.h
