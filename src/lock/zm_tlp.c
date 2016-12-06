4668b4057193a911a05d6c4042c4edbe40105cec
Halim Amer <aamer@anl.gov>
2016-12-06 15:41:46 +0000

    tlp: improve the critical path

--- src/lock/zm_tlp.c
+++ src/lock/zm_tlp.c
@@ -13,7 +13,7 @@ lock(high_p)
 do {
     if(CAS(common, FREE, GRANTED);
         break;
-    if (CAS(common, GRANTED, REQUESTED) {
+    if (CAS(common, GRANTED, REQUEST {
         while(common != GRANTED) ;
         break;     }
 } while (1)
@@ -22,7 +22,7 @@ unlock(high_p)
 CS
 
 release()
-if(!CAS(common, REQUESTED, GRANTED); //optimistic
+if(!CAS(common, REQUEST, GRANTED); //optimistic
     common = FREE
 
 acquire_low()
@@ -37,7 +37,7 @@ do {
 CS
 
 release_low()
-if(!CAS(common, REQUESTED, GRANTED); //optimistic
+if(!CAS(common, REQUEST, GRANTED); //optimistic
     common = FREE;
 unlock(low_p)
 */
@@ -95,25 +95,17 @@ int zm_tlp_init(zm_tlp_t *L)
 int zm_tlp_acquire(zm_tlp_t *L) {
     /* Acquire the high priority lock */
    zm_tlp_acquire_high_p(L);
-   do {
-        unsigned int expected = ZM_STATUS_FREE;
-        if(zm_atomic_compare_exchange_weak(&L->common,
-                                          &expected,
-                                          ZM_STATUS_GRANTED,
-                                          zm_memord_release,
-                                          zm_memord_acquire))
-            break;
-        expected = ZM_STATUS_GRANTED;
-        if(zm_atomic_compare_exchange_weak(&L->common,
-                                          &expected,
-                                          ZM_STATUS_REQUEST,
-                                          zm_memord_release,
-                                          zm_memord_acquire)) {
-            while(zm_atomic_load(&L->common, zm_memord_acquire) != ZM_STATUS_GRANTED)
+
+   unsigned int old;
+   old = zm_atomic_exchange(&L->common,
+                            ZM_STATUS_REQUEST,
+                            zm_memord_acq_rel);
+   if (old == ZM_STATUS_FREE)
+        zm_atomic_store(&L->common, ZM_STATUS_GRANTED, zm_memord_release);
+   else {
+        while(zm_atomic_load(&L->common, zm_memord_acquire) != ZM_STATUS_GRANTED)
                 ; //SPIN
-            break;
-        }
-    } while (1);
+    }
     zm_tlp_release_high_p(L);
     return 0;
 }
@@ -139,11 +131,22 @@ int zm_tlp_acquire_low(zm_tlp_t *L) {
 /* Release the lock */
 int zm_tlp_release(zm_tlp_t *L) {
     unsigned int expected = ZM_STATUS_REQUEST;
-    if(!zm_atomic_compare_exchange_weak(&L->common,
-                                        &expected,
-                                        ZM_STATUS_GRANTED,
-                                        zm_memord_release,
-                                        zm_memord_acquire))
-        zm_atomic_store(&L->common, ZM_STATUS_FREE, zm_memord_release);
+    do {
+        expected = ZM_STATUS_REQUEST;
+        if(zm_atomic_compare_exchange_weak(&L->common,
+                                           &expected,
+                                           ZM_STATUS_GRANTED,
+                                           zm_memord_release,
+                                           zm_memord_acquire))
+            break;
+
+        expected = ZM_STATUS_GRANTED;
+        if(zm_atomic_compare_exchange_weak(&L->common,
+                                           &expected,
+                                           ZM_STATUS_FREE,
+                                           zm_memord_release,
+                                           zm_memord_acquire))
+            break;
+    } while (1);
     return 0;
 }
