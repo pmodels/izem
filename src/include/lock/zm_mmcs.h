541b668f0047cafe251ca3787024191895350fcd
Halim Amer <aamer@anl.gov>
2016-12-07 06:17:12 +0000

    mmcs: add the "nowaiters" routine

--- src/include/lock/zm_mmcs.h
+++ src/include/lock/zm_mmcs.h
@@ -10,5 +10,6 @@
 int zm_mmcs_init(zm_mmcs_t *);
 int zm_mmcs_acquire(zm_mmcs_t *L, zm_mcs_qnode_t* I);
 int zm_mmcs_release(zm_mmcs_t *L);
+int zm_mmcs_nowaiters(zm_mmcs_t *L);
 
 #endif /* _ZM_MMCS_H */
