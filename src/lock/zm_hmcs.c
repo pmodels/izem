835a5ef0541ca1b51ce57dfbe08c4fba69d2249f
Halim Amer <aamer@anl.gov>
2017-10-11 03:38:00 +0000

    hmcs: initial prototype using hwloc

--- src/lock/zm_hmcs.c
+++ src/lock/zm_hmcs.c
@@ -27,10 +27,15 @@
 #include <malloc.h>
 #include <errno.h>
 #include <pthread.h>
+#include <hwloc.h>
 #include "lock/zm_lock_types.h"
 
 #ifndef DEFAULT_THRESHOLD
 #define DEFAULT_THRESHOLD 256
+#endif
+
+#ifndef HMCS_DEFAULT_MAX_LEVELS
+#define HMCS_DEFAULT_MAX_LEVELS 3
 #endif
 
 #define WAIT (0xffffffff)
@@ -84,6 +89,8 @@ struct lock{
     // Assumes tids range from [0.. max_threads)
     // Assumes that tid 0 is close to tid and so on.
     struct leaf ** leaf_nodes __attribute__((aligned(ZM_CACHELINE_SIZE)));
+    hwloc_topology_t topo;
+    int levels;
 };
 
 static zm_thread_local int tid = -1;
@@ -91,69 +98,18 @@ static zm_thread_local int tid = -1;
 /* TODO: automate hardware topology detection
  * instead of the below hard-coded method */
 
-#define IT_MACHINE
-//#define THING_MACHINE
-//#define FIRESTONE_MACHINE
-//#define LAPTOP_MACHINE
-
-#if defined(IT_MACHINE)
-static const int max_threads = 88;
-static const int levels = 3;
-static const int participants_at_level[] = {2,22,88};
-static const int thread_mappings[] = {0, 44, 1, 45, 2, 46, 3, 47, 4, 48, 5, 49, 6, 50, 7, 51, 8, 52, 9, 53, 10, 54, 11, 55, 12, 56, 13, 57, 14, 58, 15, 59, 16, 60, 17, 61, 18, 62, 19, 63, 20, 64, 21, 65, 22, 66, 23, 67, 24, 68, 25, 69, 26, 70, 27, 71, 28, 72, 29, 73, 30, 74, 31, 75, 32, 76, 33, 77, 34, 78, 35, 79, 36, 80, 37, 81, 38, 82, 39, 83, 40, 84, 41, 85, 42, 86, 43, 87};
-#elif defined(THING_MACHINE)
-static const int max_threads = 72;
-static const int levels = 3;
-static const int participants_at_level[] = {2,18,72};
-static const int thread_mappings[] = {0 , 36 , 1 , 37 , 2 , 38 , 3 , 39 , 4 , 40 , 5 , 41 , 6 , 42 , 7 , 43 , 8 , 44 , 9 , 45 , 10 , 46 , 11 , 47 , 12 , 48 , 13 , 49 , 14 , 50 , 15 , 51 , 16 , 52 , 17 , 53 , 18 , 54 , 19 , 55 , 20 , 56 , 21 , 57 , 22 , 58 , 23 , 59 , 24 , 60 , 25 , 61 , 26 , 62 , 27 , 63 , 28 , 64 , 29 , 65 , 30 , 66 , 31 , 67 , 32 , 68 , 33 , 69 , 34 , 70 , 35 , 71};
-#elif defined(FIRESTONE_MACHINE)
-static const int max_threads = 160;
-static const int levels = 3;
-static const int participants_at_level[] = {8,80,160};
-static const int thread_mappings[] = {0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 , 21 , 22 , 23 , 24 , 25 , 26 , 27 , 28 , 29 , 30 , 31 , 32 , 33 , 34 , 35 , 36 , 37 , 38 , 39 , 40 , 41 , 42 , 43 , 44 , 45 , 46 , 47 , 48 , 49 , 50 , 51 , 52 , 53 , 54 , 55 , 56 , 57 , 58 , 59 , 60 , 61 , 62 , 63 , 64 , 65 , 66 , 67 , 68 , 69 , 70 , 71 , 72 , 73 , 74 , 75 , 76 , 77 , 78 , 79 , 80 , 81 , 82 , 83 , 84 , 85 , 86 , 87 , 88 , 89 , 90 , 91 , 92 , 93 , 94 , 95 , 96 , 97 , 98 , 99 , 100 , 101 , 102 , 103 , 104 , 105 , 106 , 107 , 108 , 109 , 110 , 111 , 112 , 113 , 114 , 115 , 116 , 117 , 118 , 119 , 120 , 121 , 122 , 123 , 124 , 125 , 126 , 127 , 128 , 129 , 130 , 131 , 132 , 133 , 134 , 135 , 136 , 137 , 138 , 139 , 140 , 141 , 142 , 143 , 144 , 145 , 146 , 147 , 148 , 149 , 150 , 151 , 152 , 153 , 154 , 155 , 156 , 157 , 158 , 159};
-#elif defined(LAPTOP_MACHINE)
-static const int max_threads = 4;
-static const int levels = 2;
-static const int participants_at_level[] = {2,4};
-static const int thread_mappings[] = {0 , 1 , 2 , 3};
-#else
-#error "Machine topology not recognized"
-#endif
-
-#define handle_error_en(en, msg) \
-do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
-
-static void set_affinity(int tid){
-    int s, j;
-    cpu_set_t cpuset;
-    pthread_t thread;
-
-    thread = pthread_self();
-
-    /* Set affinity mask to include CPUs tid */
-
-    CPU_ZERO(&cpuset);
-    CPU_SET(1*tid, &cpuset);
-
-    s = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
-    if (s != 0)
-        handle_error_en(s, "pthread_setaffinity_np");
-}
-
 /* Check the actual affinity mask assigned to the thread */
-static void check_affinity(int tid) {
-    int s, j;
-    cpu_set_t cpuset;
-    pthread_t thread = pthread_self();
-    s = pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
-    if (s != 0)
-        handle_error_en(s, "pthread_getaffinity_np");
+static void check_affinity(hwloc_topology_t topo) {
+    hwloc_cpuset_t cpuset = hwloc_bitmap_alloc();
+    int set_length;
+    hwloc_get_cpubind(topo, cpuset, HWLOC_CPUBIND_THREAD);
+    set_length = hwloc_get_nbobjs_inside_cpuset_by_type(topo, cpuset, HWLOC_OBJ_PU);
+    hwloc_bitmap_free(cpuset);
 
-    int num_hw_threads = 0;
-    for (j = 0; j < CPU_SETSIZE; j++)
-        if (CPU_ISSET(j, &cpuset))
-            num_hw_threads++;
-    assert(num_hw_threads==1);
+    if(set_length != 1) {
+        printf("IZEM:HMCS:ERROR: thread bound to more than one HW thread!\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 static inline void reuse_qnode(zm_mcs_qnode_t *I){
@@ -315,7 +271,7 @@ static void* new_leaf(struct hnode *h, int depth) {
     return leaf;
 }
 
-static inline void acquire_from_leaf(struct leaf *L){
+static inline void acquire_from_leaf(int level, struct leaf *L){
     if((zm_ptr_t)L->cur_node->lock == ZM_NULL
     && (zm_ptr_t)L->root_node->lock == ZM_NULL) {
         // go FP
@@ -323,50 +279,89 @@ static inline void acquire_from_leaf(struct leaf *L){
         acquire_root(L->root_node, &L->I);
         return;
     }
-    acquire_helper(levels, L->cur_node, &L->I);
+    acquire_helper(level, L->cur_node, &L->I);
     return;
 }
 
-static inline void release_from_leaf(struct leaf *L){
+static inline void release_from_leaf(int level, struct leaf *L){
     //myrelease(cur_node, I);
     if(L->took_fast_path) {
         release_root(L->root_node, &L->I);
         L->took_fast_path = FALSE;
         return;
     }
-    release_helper(levels, L->cur_node, &L->I);
+    release_helper(level, L->cur_node, &L->I);
     return;
 }
 
-static inline int nowaiters_from_leaf(struct leaf *L){
+static inline int nowaiters_from_leaf(int level, struct leaf *L){
     // Shouldnt this be nowaiters(root_node, I)?
     if(L->took_fast_path) {
         return nowaiters_root(L->cur_node, &L->I);
     }
 
-    return nowaiters_helper(levels, L->cur_node, &L->I);
+    return nowaiters_helper(level, L->cur_node, &L->I);
 }
 
-static int get_hwthread_id(int id){
-   for(int i = 0 ; i < max_threads; i++)
-       if(id == thread_mappings[i])
-            return i;
+static int get_hwthread_id(hwloc_topology_t topo){
+    hwloc_cpuset_t cpuset = hwloc_bitmap_alloc();
+    hwloc_obj_t obj;
+    hwloc_get_cpubind(topo, cpuset, HWLOC_CPUBIND_THREAD);
+    obj = hwloc_get_obj_inside_cpuset_by_type(topo, cpuset, HWLOC_OBJ_PU, 0);
+    hwloc_bitmap_free(cpuset);
+    return obj->logical_index;
+}
 
-   assert(0 && "Should never reach here");
+static void set_hierarchy(struct lock *L, int *max_threads, int** particip_per_level) {
+    int max_depth, levels = 0, max_levels = HMCS_DEFAULT_MAX_LEVELS;
+
+    hwloc_topology_init(&L->topo);
+    hwloc_topology_load(L->topo);
+
+    *max_threads = hwloc_get_nbobjs_by_type(L->topo, HWLOC_OBJ_PU);
+
+    max_depth = hwloc_topology_get_depth(L->topo);
+    int nobjs_per_level[max_depth];
+
+    int prev_nobjs = -1;
+    for (int d = max_depth - 1; d > 0; d--) {
+        int cur_nobjs = hwloc_get_nbobjs_by_depth(L->topo, d);
+        /* Check if this level has a hierarchical impact */
+        if(cur_nobjs != prev_nobjs) {
+            prev_nobjs = cur_nobjs;
+            nobjs_per_level[levels] = cur_nobjs;
+            levels++;
+            if(levels >= max_levels)
+                break;
+        }
+    }
+
+    *particip_per_level = (int*) malloc(levels * sizeof(int));
+    for(int level = 0; level < levels; level++)
+        (*particip_per_level)[level] = nobjs_per_level[levels - (level + 1)];
+
+    L->levels = levels;
+}
+
+static void free_hierarchy(int* particip_per_level){
+    free(particip_per_level);
 }
 
 static void* new_lock(){
 
     struct lock *L = (struct lock*)memalign(ZM_CACHELINE_SIZE, sizeof(struct lock));
 
+    int max_threads;
+    int *participants_at_level;
+    set_hierarchy(L, &max_threads, &participants_at_level);
+
     // Total locks needed = participantsPerLevel[1] + participantsPerLevel[2] + .. participantsPerLevel[levels-1] + 1
     // Save affinity
-    pthread_t thread = pthread_self();
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+    hwloc_cpuset_t cpuset = hwloc_bitmap_alloc();
+    hwloc_get_cpubind(L->topo, cpuset, HWLOC_CPUBIND_THREAD);
 
     int total_locks_needed = 0;
+    int levels = L->levels;
 
     for (int i=0; i < levels; i++) {
         total_locks_needed += max_threads / participants_at_level[i] ;
@@ -379,8 +374,10 @@ static void* new_lock(){
     if (s != NULL)
         threshold = atoi(s);
 
+    hwloc_obj_t obj;
     for(int tid = 0 ; tid < max_threads; tid ++){
-        set_affinity(thread_mappings[tid]);
+        obj = hwloc_get_obj_by_type (L->topo, HWLOC_OBJ_PU, tid);
+        hwloc_set_cpubind(L->topo, obj->cpuset, HWLOC_CPUBIND_THREAD);
         // Pin me to hw-thread-id = tid
         int last_lock_location_end = 0;
         for(int cur_level = 0 ; cur_level < levels; cur_level++){
@@ -399,7 +396,8 @@ static void* new_lock(){
 
     // setup parents
     for(int tid = 0 ; tid < max_threads; tid ++){
-        set_affinity(thread_mappings[tid]);
+        hwloc_get_obj_by_type (L->topo, HWLOC_OBJ_PU, tid);
+        hwloc_set_cpubind(L->topo, obj->cpuset, HWLOC_CPUBIND_THREAD);
         int last_lock_location_end = 0;
         for(int cur_level = 0 ; cur_level < levels - 1; cur_level++){
             if (tid%participants_at_level[cur_level] == 0) {
@@ -412,8 +410,9 @@ static void* new_lock(){
         leaf_nodes[tid] = (struct leaf*)new_leaf(lock_locations[tid/participants_at_level[0]], levels);
     }
     free(lock_locations);
+    free_hierarchy(participants_at_level);
     // Restore affinity
-    pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+    hwloc_set_cpubind(L->topo, cpuset, HWLOC_CPUBIND_THREAD);
     L->leaf_nodes = leaf_nodes;
 
     return L;
@@ -421,19 +420,18 @@ static void* new_lock(){
 
 static inline void hmcs_acquire(struct lock *L){
     if (zm_unlikely(tid == -1)) {
-        tid = sched_getcpu();
-        check_affinity(tid);
-        tid = get_hwthread_id(tid);
+        check_affinity(L->topo);
+        tid = get_hwthread_id(L->topo);
     }
-    acquire_from_leaf(L->leaf_nodes[tid]);
+    acquire_from_leaf(L->levels, L->leaf_nodes[tid]);
 }
 
 static inline void hmcs_release(struct lock *L){
-    release_from_leaf(L->leaf_nodes[tid]);
+    release_from_leaf(L->levels, L->leaf_nodes[tid]);
 }
 
 static inline int hmcs_nowaiters(struct lock *L){
-    return nowaiters_from_leaf(L->leaf_nodes[tid]);
+    return nowaiters_from_leaf(L->levels, L->leaf_nodes[tid]);
 }
 
 
